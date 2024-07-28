3567207756d903cd0e5bbcdba7b94f0e6819da18
Alidiamond <107695618+alidiamond1@users.noreply.github.com>
2024-07-28 03:51:02 -0700

    Add files via upload

---
+++ EventSchedule/EventSchedule/EventSchedule/src/main/java/com/eventschadule/eventschadule/model/Event.java
@@ -0,0 +1,36 @@
+package com.eventschadule.eventschadule.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    // Getters and setters
+    // Constructor
+    // toString method
+}
